6cfd4c3f61f20a8c5d398cda5913bde765bdbebe
Jeremia Manogi Mario <jeremimanogi@gmail.com>
2022-05-27 23:19:30 +0700

    add param image

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -2,6 +2,7 @@ class Product {
   final String name;
   final String description;
   final String price;
+  final String image;
 
-  Product(this.name, this.description, this.price);
+  Product(this.name, this.description, this.price, this.image);
 }
