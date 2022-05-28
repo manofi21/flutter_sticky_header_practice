48cf8186ed9b49fb652c20aaf7ed00fe3a2e320f
Jeremia Manogi Mario <jeremimanogi@gmail.com>
2022-05-28 08:43:09 +0700

    update list_item_header_sliver: - wrap row with ValueListenableBuilder - add controller from bloc - wrap container with GetBoxOffset

--- lib/src/list_item_header_sliver.dart
+++ lib/src/list_item_header_sliver.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sticky_header_practice/model/models.dart';
+import 'package:flutter_sticky_header_practice/src/get_box_offset.dart';
 
 import '../controller/sliver_scroll_controller.dart';
 
@@ -11,33 +13,45 @@ class ListItemHeaderSliver extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final itemsOffset = bloc.listOffsetItemHeader;
     return Padding(
       padding: const EdgeInsets.only(left: 16.0),
       child: SingleChildScrollView(
+        controller: bloc.scrollControllerItemHeader,
         physics: const NeverScrollableScrollPhysics(),
         scrollDirection: Axis.horizontal,
-        child: Row(
-          children: List.generate(bloc.listCategory.length, (index) {
-            return Container(
-              margin: const EdgeInsets.only(
-                top: 8,
-                bottom: 8,
-                right: 8,
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: index == 0 ? Colors.white : null,
-                  borderRadius: BorderRadius.circular(16)),
-              child: Text(
-                bloc.listCategory[index].category,
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  color: index == 0 ? Colors.black : Colors.white,
-                ),
-              ),
+        child: ValueListenableBuilder(
+          valueListenable: bloc.headerNotifier,
+          builder: (_, MyHeader? snapshot, __) {
+            return Row(
+              children: List.generate(bloc.listCategory.length, (index) {
+                return GetBoxOffset(
+                  offset: ((offset) => itemsOffset[index] = offset.dx),
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      top: 8,
+                      bottom: 8,
+                      right: 8,
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: index == snapshot!.index ? Colors.white : null,
+                        borderRadius: BorderRadius.circular(16)),
+                    child: Text(
+                      bloc.listCategory[index].category,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: index == snapshot.index
+                            ? Colors.black
+                            : Colors.white,
+                      ),
+                    ),
+                  ),
+                );
+              }),
             );
-          }),
+          },
         ),
       ),
     );
