9c8c36a363cf6795f676665a9731f26d57a26de8
Jeremia Manogi Mario <jeremimanogi@gmail.com>
2022-05-28 09:38:52 +0700

    add temporary dummy data

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,11 +1,58 @@
 import 'package:flutter_sticky_header_practice/model/product_category.dart';
 
-final product = [
+final products = [
   Product(
     name: 'Choclate Cake',
     image: 'assets/japan_backImage.png',
     description: 'hai,.,,,,.......................................',
     price: '\$19',
-    
+  ),
+  Product(
+    name: 'Choclate Cake',
+    image: 'assets/japan_backImage.png',
+    description: 'hai,.,,,,.......................................',
+    price: '\$19',
+  ),
+  Product(
+    name: 'Choclate Cake',
+    image: 'assets/japan_backImage.png',
+    description: 'hai,.,,,,.......................................',
+    price: '\$19',
+  ),
+  Product(
+    name: 'Choclate Cake',
+    image: 'assets/japan_backImage.png',
+    description: 'hai,.,,,,.......................................',
+    price: '\$19',
+  ),
+  Product(
+    name: 'Choclate Cake',
+    image: 'assets/japan_backImage.png',
+    description: 'hai,.,,,,.......................................',
+    price: '\$19',
+  ),
+  Product(
+    name: 'Choclate Cake',
+    image: 'assets/japan_backImage.png',
+    description: 'hai,.,,,,.......................................',
+    price: '\$19',
+  ),
+  Product(
+    name: 'Choclate Cake',
+    image: 'assets/japan_backImage.png',
+    description: 'hai,.,,,,.......................................',
+    price: '\$19',
+  ),
+  Product(
+    name: 'Choclate Cake',
+    image: 'assets/japan_backImage.png',
+    description: 'hai,.,,,,.......................................',
+    price: '\$19',
+  ),
+  Product(
+    name: 'Choclate Cake',
+    image: 'assets/japan_backImage.png',
+    description: 'hai,.,,,,.......................................',
+    price: '\$19',
   ),
 ];
